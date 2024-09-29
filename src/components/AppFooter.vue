83ef0cc1041cb17a643981c5cf4d1ce6636b74ba
Byron Hall <byronhall@Byrons-Mac-mini.local>
2024-09-29 18:22:38 -0500

    initial commit

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-footer height="40" app>
+    <a v-for="item in items" :key="item.title" :href="item.href" :title="item.title"
+      class="d-inline-block mx-2 social-link" rel="noopener noreferrer" target="_blank">
+      <v-icon :icon="item.icon" :size="item.icon === '$vuetify' ? 24 : 16" />
+    </a>
+
+    <div class="text-caption text-disabled" style="position: absolute; right: 16px;">
+      &copy; 2024-{{ (new Date()).getFullYear() }} | <span class="d-none d-sm-inline-block">232 Publishing</span>
+    </div>
+  </v-footer>
+</template>
+
+<script setup>
+const items = [
+  {
+    title: 'iam@byronjmil.com',
+    icon: 'mdi-email',
+    href: 'mailto:iam@byronjmil.com',
+  },
+]
+</script>
+
+<style scoped lang="sass">
+  .social-link :deep(.v-icon)
+    color: rgba(var(--v-theme-on-background), var(--v-disabled-opacity))
+    text-decoration: none
+    transition: .2s ease-in-out
+
+    &:hover
+      color: rgba(25, 118, 210, 1)
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-container class="fill-height">
+    <v-responsive class="align-centerfill-height mx-auto" max-width="900">
+      <div class="text-h5 text-center">Last Update: September 29, 2024</div>
+      <v-carousel hide-delimiters>
+        <v-carousel-item v-for="number in numbersArray" :key="number" :src="images[number]"
+          alt="35mm Photo"></v-carousel-item>
+      </v-carousel>
+    </v-responsive>
+  </v-container>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue';
+
+const numberOfImages = 124;
+const uInt8Array = new Uint8Array(numberOfImages);
+window.crypto.getRandomValues(uInt8Array);
+const numbersArray = Array.from({ length: numberOfImages }, (_, i) => i);
+
+// Shuffle the numbersArray
+for (let i = 0; i < numberOfImages; i += 1) {
+  let swapIndex = uInt8Array[i] % numberOfImages;
+  let swapValue = numbersArray[swapIndex];
+  numbersArray[swapIndex] = numbersArray[i];
+  numbersArray[i] = swapValue;
+}
+const images = ref({});
+
+onMounted(async () => {
+  const imagePromises = numbersArray.map(async (number) => {
+    try {
+      const image = await import(`@/assets/images/${number}.jpeg`);
+      images.value[number] = image.default; // Store image URL in the object
+    } catch (error) {
+      console.error(`Error loading image ${number}:`, error);
+    }
+  });
+
+  await Promise.all(imagePromises); // Wait for all images to load
+});
+</script>
